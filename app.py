b350cbbce24bb64f198967fd88a9611dcbbc6d03
umangm0389-sketch <umangm0389@gmail.com>
2025-12-31 10:07:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Cinema Hub Stream Site Running"
+    return render_template("watch.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
