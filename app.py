fcd4bf2ad9f1cf3eeba04090568791193b1199db
umangm0389-sketch <umangm0389@gmail.com>
2025-12-31 10:18:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("watch.html")
+def watch():
+    video = request.args.get("video")
+    return render_template("watch.html", video=video)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
